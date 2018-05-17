1f93f3aafb54644f45051761efd44b89c1833a1f
Kurt Googler Kanaskie <kurtkanaskie@google.com>
2018-05-17 12:55:20 -0400

    Added script to set env variables from branch

---
+++ set-env-values.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+# If GIT_BRANCH is master or feature set EdgeEnv to "test"
+# If GIT_BRANCH is feature set EdgeDeploySuffix to featurename
+# /origin/master
+# /origin/feature/jira1
+# /origin/prod
+
+echo BRANCH: $GIT_BRANCH
+
+EdgeEnv="" 
+EdgeDeploySuffix="" 
+
+if [[ "$GIT_BRANCH" == /origin/master ]]
+then
+	export EdgeEnv="test"
+elif [[ "$GIT_BRANCH" = /origin/feature/* ]]
+then
+	export EdgeEnv="test"
+	export EdgeDeploySuffix=${GIT_BRANCH#/origin/feature/}
+elif [[ "$GIT_BRANCH" == /origin/prod ]]
+then
+	export EdgeEnv="prod"
+else
+	echo BRANCH PATH NOT FOUND
+	exit 1
+fi
+
+echo EdgeEnv: $EdgeEnv
+echo EdgeDeploySuffix: $EdgeDeploySuffix
+
